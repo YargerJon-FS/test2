bca20f9322a5ec0bb016eb0e3d6f6238f2b0ef5a
YargerJon-FS <jlyarger@student.fullsail.edu>
2022-07-03 18:05:03 -0400

    Updated server.js, attempted assignment 1.5- added POST,PATCH,DELTE but metadata is an issue will need some help

--- server.js
+++ server.js
@@ -4,7 +4,38 @@ const app = express();
 
 app.get("/", (req, res, next) => {
     res.json({
-        message: "Did you GET IT?"
+        message: "Using GET /",
+        metadata: {
+            
+        }
+    });
+});
+
+//post, patch, delete
+app.post("/", (req, res, next) => {
+    res.json({
+        message: "Using POST /",
+        metadata: {
+            
+        }
+    });
+});
+
+app.patch("/", (req, res, next) => {
+    res.json({
+        message: "Using PATCH /",
+        metadata: (env.port)
+            
+        
+    });
+});
+
+app.delete("/", (req, res, next) => {
+    res.json({
+        message: "Using delete /",
+        metadata: {
+            
+        }
     });
 });
 
@@ -14,6 +45,24 @@ app.use((req, res, next) => {
     error.status= 404;
     next(error);
 });
+app.use((req, res, next) => {
+    const post = new Post ("Using Post");
+    post.status = "Port: 3000, Host: localhost";
+    next(post);
+
+});
+// app.use((req, res, next) => {
+//     const delete = new Delete ("Using Delete");
+//     delete.status = "Port:3000, Host:localhost";
+//     next(delete);
+
+// });
+app.use((req, res, next) => {
+    const patch = new Patch ("Using Patch");
+    patch.status = "Port: 3000, Host: localhost";
+    next(patch);
+
+})
 // middleware to send error nicely
 app.use((error, req,res,next) =>{
     res.status(error.status || 500).json({
@@ -24,5 +73,15 @@ app.use((error, req,res,next) =>{
         }
     });
 });
+// app.use((delete, req,res,next) =>{
+//     res.status(delete.status || 500).json({
+//         error: {
+//             message: delete.message, 
+//             status: delete.status,
+//             method: req.method
+//         }
+//     });
+// });
+
 
 app.listen(process.env.port, () => console.log(`Starting serving on port ${process.env.port}`))
